457d01995380a509367a49f41a2488aedded0bfa
Dhruv Dashora <f20231190@goa.bits-pilani.ac.in>
2024-02-21 23:24:21 +0530

    final

---
+++ lib/Firestore.dart
@@ -0,0 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirestoreServices {
+  static Future<void> saveUser(String name, String email, String uid) async {
+    final FirebaseFirestore firestore = FirebaseFirestore.instance;
+    await firestore.collection('users').doc(uid).set({'email': email, 'name': name});
+  }
+}
+
+
+
+
